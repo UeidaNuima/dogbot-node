2b3366b8687742c45b81c5be8fe666ac3eb349f4
MoonDropX <minalinskyx@hotmail.com>
2018-12-07 22:37:20 +0800

    feat(middlewares/help): add new instructions

--- src/middleware/help.ts
+++ src/middleware/help.ts
@@ -1,21 +1,28 @@
 import { Context } from 'dogq';
 
 export default async (ctx: Context) => {
-  const helper = `ケルベロス ver4.0.0
+  const helper = `ケルベロス ver4.1.0
   /twitter [几天以前]
-  /exp <稀有度> <初始等级>|<x桶|x祝福> <目标等级>
-  /emoji add <名称> <图片>
-         del <名称> [序号]
-         name  add <名称> <新名称>
-               del <名称> [序号]
-         group add <名称> [群号]
-               del <名称> [群号]
-         list <名称>
+  /exp 稀有度 初始等级 目标等级
+  /exp 稀有度 x桶|x祝福[+x桶|+x祝福] 目标等级
+  /emoji add 名称 图片
+         del 名称 [序号]
+         name  add 名称 新名称
+               del 名称 [序号]
+         group add 名称 [群号]
+               del 名称 [群号]
+         list 名称
   /poster [更新星期]
+  /status [-f] 单位
+  /conne [-s sorter] 单位|职业 [单位|职业 [...]]
+  /material [-r] 职业
   /help
 ----
 桶 => /exp
 推特 => /twitter
-海报 => /poster`;
+海报 => /poster
+属性图 => /status
+圆爹 => /conne
+素材 => /material`;
   ctx.reply(helper);
 };
